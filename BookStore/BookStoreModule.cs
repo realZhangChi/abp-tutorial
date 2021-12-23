1512c979cec6934aad3ac9641205d56e9cbcc662
Chi Zhang <zliming@outlook.com>
2021-12-23 13:36:22 +0800

    add swagger

---
+++ BookStore/BookStoreModule.cs
@@ -0,0 +1,38 @@
+﻿using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace BookStore;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpAspNetCoreMvcModule))]
+public class BookStoreModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        ConfigureSwaggerServices(context);
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+
+    
+    private static void ConfigureSwaggerServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSwaggerGen();
+    }
+}
